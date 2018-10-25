d796497dd076eac580a3bc7f0b7c21dd3b0cb425
Fgyter <fgyterr@yandex.ru>
2018-10-25 11:31:42 +0300

    lesson5

---
+++ lesson4/variables.rb
@@ -0,0 +1,14 @@
+hello = "Привет, пацаны!"
+#hello обьект типа строка, = присвоить переменную обьекту, "Привет, пацаны!" сама переменная
+hello2 = nil
+hello2 = "пока девченки"
+
+devchenki = true
+
+if (devchenki == true)
+	puts hello2
+else
+	puts hello
+end
+
+puts "досвидание!"
